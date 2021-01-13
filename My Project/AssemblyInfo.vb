14fdc91158fba0ced1663efa5127740be383b7ec
Gilcspen@gmail.com <Gilcspen@gmail.com>
2021-01-13 08:34:11 -0700

    Add project files.

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Operators")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP Inc.")>
+<Assembly: AssemblyProduct("Operators")>
+<Assembly: AssemblyCopyright("Copyright © HP Inc. 2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("f544f5f4-5976-4b50-95e3-d124e47c07d9")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ Operators.vb
@@ -0,0 +1,21 @@
+﻿Module Operators
+
+    Sub Main()
+
+        Console.WriteLine(2 + 2)
+        Console.WriteLine(2 - 2)
+        Console.WriteLine(2 * 2)
+
+        'This Literally just does the math for you.
+
+        Console.WriteLine(5 / 25)
+        Console.WriteLine(1 Mod 2) 'Mod just takes the Remainder
+        Console.WriteLine(3 * 2)
+
+
+        Console.ReadLine()
+
+
+    End Sub
+
+End Module
